277b9b29423c0ff8127a6db0287d88930ffdcf39
Tuong1994 <77843790+Tuong1994@users.noreply.github.com>
2026-06-25 06:55:20 +0000

    updating product-filter

---
+++ src/features/management/components/storage/product-filter-mobile.tsx
@@ -0,0 +1,13 @@
+import type { ProductDataTable } from "@/models/product/product.type"
+import type { Table } from "@tanstack/react-table"
+import type { FC } from "react"
+
+interface ProductFilterMobileProps {
+  table: Table<ProductDataTable>
+}
+
+const ProductFilterMobile: FC<ProductFilterMobileProps> = () => {
+  return <></>
+}
+
+export default ProductFilterMobile
