f85ad12debb36e606c6a4d020a783c46506adb64
Tuong1994 <nhambontuong68@gmail.com>
2026-07-01 07:33:00 +0700

    created modal-form

---
+++ src/components/page/modal-form/index.tsx
@@ -0,0 +1,23 @@
+import type { FC } from "react"
+import { Dialog as DialogPrimitive } from "@base-ui/react/dialog"
+import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog"
+
+interface ModalFormProps extends DialogPrimitive.Root.Props {
+  chidlren?: ReactNode
+  header?: ReactNode
+}
+
+const ModalForm: FC<ModalFormProps> = ({ children, header, ...restProps }) => {
+  return (
+    <Dialog {...restProps}>
+      <DialogContent className="p-0 py-2 md:min-w-150 lg:min-w-200">
+        <DialogHeader className="p-4 pb-0 text-lg font-bold">{header}</DialogHeader>
+        <div className="no-scrollbar max-h-[70vh] overflow-y-auto p-4 pt-1.5 lg:max-h-full lg:overflow-y-visible">
+          {children}
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default ModalForm
--- src/features/management/components/storage/product-form/index.tsx
+++ src/features/management/components/storage/product-form/index.tsx
@@ -1,6 +1,5 @@
 import type { FC } from "react"
-import { Dialog as DialogPrimitive } from "@base-ui/react/dialog"
-import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog"
+import ModalForm from "@/components/page/modal-form"
 import FormLayout from "@/components/page/form-layout"
 import ProductFormLeft from "./product-form-left"
 import ProductFormRight from "./product-form-right"
@@ -12,14 +11,9 @@ const ProductsForm: FC<ProductsFormProps> = ({ ...restProps }) => {
   const { lang } = useLocale()
 
   return (
-    <Dialog {...restProps}>
-      <DialogContent className="p-0 py-2 md:min-w-150 lg:min-w-200">
-        <DialogHeader className="p-4 pb-0 text-lg font-bold">{lang.management.storage.productForm.title}</DialogHeader>
-        <div className="no-scrollbar max-h-[70vh] overflow-y-auto p-4 pt-1.5 lg:max-h-full lg:overflow-y-visible">
-          <FormLayout left={<ProductFormLeft />} right={<ProductFormRight />} />
-        </div>
-      </DialogContent>
-    </Dialog>
+    <ModalForm {...restProps} header={lang.management.storage.productForm.title}>
+      <FormLayout left={<ProductFormLeft />} right={<ProductFormRight />} />
+    </ModalForm>
   )
 }
 
