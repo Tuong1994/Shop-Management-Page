cbd919ac329e67ac6e529a6d026d9142023be66e
Tuong1994 <77843790+Tuong1994@users.noreply.github.com>
2026-07-02 06:17:55 +0000

    updating hiring page

--- src/features/management/components/staffs/hiring-list/hiring-list-card/card-modal.tsx
+++ src/features/management/components/staffs/hiring-list/hiring-list-card/card-modal.tsx
@@ -1,13 +1,24 @@
-import type { FC } from "react";
-import { Dialog as DialogPrimitive } from "@base-ui/react/dialog";
-import { Dialog, DialogContent } from "@/components/ui/dialog";
+import type { FC } from "react"
+import { Dialog as DialogPrimitive } from "@base-ui/react/dialog"
+import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog"
+import InfoRow from "@/components/page/info-row"
+import useLocale from "@/locale/use-locale"
 
 interface HiringCardModalProps extends DialogPrimitive.Root.Props {}
 
-const HiringCardModal: FC<HiringCardModalProps> = ({...restProps}) => {
-    return <Dialog {...restProps}>
-        <DialogContent></DialogContent>
+const HiringCardModal: FC<HiringCardModalProps> = ({ ...restProps }) => {
+  const { lang } = useLocale()
+
+  return (
+    <Dialog {...restProps}>
+      <DialogContent>
+        <DialogHeader>Candidate profile</DialogHeader>
+        <div className="">
+          <InfoRow name={lang.common.form.label.fullName} descript="Cadidate name" />
+        </div>
+      </DialogContent>
     </Dialog>
+  )
 }
 
 export default HiringCardModal
