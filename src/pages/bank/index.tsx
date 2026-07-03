8f4afe090ce50cdb7e5b9b47772129a60566a91f
Tuong1994 <77843790+Tuong1994@users.noreply.github.com>
2026-07-03 02:30:51 +0000

    bank page init

--- src/pages/bank/index.tsx
+++ src/pages/bank/index.tsx
@@ -1,7 +1,11 @@
 import type { FC } from "react"
+import { EPageType } from "@/data/page"
+import ContentLayout from "@/components/page/content-layout"
 
 const BankPage: FC = () => {
-  return <div>Bank Page</div>
+  return <ContentLayout pageType={EPageType.BANK}>
+    Bank page
+  </ContentLayout>
 }
 
 export default BankPage
